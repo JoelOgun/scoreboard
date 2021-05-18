bc3c0c4ada1dd890c48aff798b2ce7f493106547
Joel Ogunremi <joelogunremi@gmail.com>
2021-05-17 21:59:40 -0400

    add counter component and app component

--- app.js
+++ app.js
@@ -11,13 +11,28 @@ const Player = () => {
   return (
     <div className="player">
       <span className="player-name">Joel</span>
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score">35</span>
-        <button className="counter-action increment"> + </button>
-      </div>
+      <Counter />
+    </div>
+  );
+};
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+
+      {/* Players list */}
+      <Player />
     </div>
   );
 };
 
-ReactDOM.render(<Player />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
