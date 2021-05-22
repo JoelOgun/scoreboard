7d38197bd89575ef655801c080288a827a48f54e
Joel Ogunremi <joelogunremi@gmail.com>
2021-05-22 16:06:03 -0400

    Create class and stateful component for Counter

--- app.js
+++ app.js
@@ -34,19 +34,25 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">{props.name}</span>
-      <Counter score={props.score} />
+      <Counter />
     </div>
   );
 };
-const Counter = (props) => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{props.score}</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-};
+
+class Counter extends React.Component {
+  state = {
+    score: 0,
+  };
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+}
 const App = (props) => {
   return (
     <div className="scoreboard">
@@ -57,11 +63,7 @@ const App = (props) => {
 
       {/* Players list */}
       {props.initialPlayers.map((player) => (
-        <Player
-          name={player.name}
-          score={player.score}
-          key={player.id.toString()}
-        />
+        <Player name={player.name} key={player.id.toString()} />
       ))}
     </div>
   );
